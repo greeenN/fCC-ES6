c6fd68588372e9833934e33db8fd0f65bb65056a
greeenN <nikgrishin1999@mail.ru>
2021-11-03 19:29:14 +0300

    "3"

--- script.js
+++ script.js
@@ -24,3 +24,21 @@ function editInPlace() {
     // Only change code above this line
 }
 editInPlace();*/
+
+// 3
+
+/*function freezeObj() {
+    const MATH_CONSTANTS = {
+        PI: 3.14
+    };
+    // Only change code below this line
+    Object.freeze(MATH_CONSTANTS);
+    // Only change code above this line
+    try {
+        MATH_CONSTANTS.PI = 99;
+    } catch(ex) {
+        console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();*/
