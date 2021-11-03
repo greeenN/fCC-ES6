c5261a689277a5a17af592d16ab29a45630350c6
greeenN <nikgrishin1999@mail.ru>
2021-11-03 19:26:41 +0300

    "2"

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ function checkScope() {
 }*/
 
 // 2
+
+/*
+const s = [5, 7, 2];
+function editInPlace() {
+    // Only change code below this line
+
+    s[0] = 2;
+    s[1] = 5;
+    s[2] = 7;
+    // Only change code above this line
+}
+editInPlace();*/
